73be79aa31082c9b7bc39be350ca7573d68d77ad
olegletto <olegletto@gmail.com>
2020-01-28 19:46:20 +0300

    add task5

--- Lesson 1/Task4.py
+++ Lesson 1/Task4.py
@@ -1,9 +1,10 @@
-l = ['abc', 'xyz', 'aba', '1221']
+l = ['abc', 'xyz', 'aba', '1221', 'anna']
 c = 0
-
-i=0
-while i < len(l)
-    
-c += 1
+i = 0
+while i < len(l):
+    s = l[i]
+    if s[0] == s[-1]:
+        c += 1
+    i += 1
 
 print (c)
---
+++ Lesson 1/Task5.py
@@ -0,0 +1,5 @@
+a = set([1,2])
+b = set([2,3])
+c = set([2])
+s = c <= a & c <= b
+print (s)
